4892e7c8c3d6feb9937deeb15b5c8b4f602c1234
annie <10756031@ntub.edu.tw>
2023-11-23 00:09:56 +0800

    page_update

--- views/example_views.py
+++ views/example_views.py
@@ -33,15 +33,47 @@ def theme(request):
 
 def pic(request):
     return render(request, 'pic.html')
+def pic1(request):
+    return render(request, 'pic1.html')
 
 def reading(request):
     return render(request, 'reading.html')
 
+def reading1(request):
+    return render(request, 'reading1.html')
+
+def reading2(request):
+    return render(request, 'reading2.html')
+def reading3(request):
+    return render(request, 'reading3.html')
+def reading4(request):
+    return render(request, 'reading4.html')
+def reading5(request):
+    return render(request, 'reading5.html')
 def gtp_reading(request):
     return render(request, 'gtp_reading.html')
-
+def gtp_reading1(request):
+    return render(request, 'gtp_reading1.html')
+def gtp_reading2(request):
+    return render(request, 'gtp_reading2.html')
+def gtp_reading3(request):
+    return render(request, 'gtp_reading3.html')
+def gtp_reading4(request):
+    return render(request, 'gtp_reading4.html')
+def gtp_reading5(request):
+    return render(request, 'gtp_reading5.html')
 def score(request):
     return render(request, 'score.html')
+def score1(request):
+    return render(request, 'score1.html')
+def score2(request):
+    return render(request, 'score2.html')
+def score3(request):
+    return render(request, 'score3.html')
+def score4(request):
+    return render(request, 'score4.html')
+def score5(request):
+    return render(request, 'score5.html')
 
 def vocabulary(request):
     return render(request, 'vocabulary.html')
